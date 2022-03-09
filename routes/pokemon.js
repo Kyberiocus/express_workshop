a9ef14ed8d65f01ccd0d74b8805097f9ac8783c7
Kyberiocus <jdanielv00@gmail.com>
2022-03-09 16:50:23 -0600

    change body parser

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,17 +8,17 @@ pokemon.post("/", (req, res, next) => {
 
 pokemon.get("/", async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 })
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id);
-    if(id >= 0 && id <= 722){
+    if(id >= 1 && id <= 722){
         console.log(id);
-        return res.status(200).json(pkmn);
+        return res.status(200).json({code: 1, message: pkmn});
     }
-    return res.status(404).send("Pokemon no encontrado");
+    return res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
@@ -29,7 +29,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
         //return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
     //});
 
-    (pkmn.length > 0) ? res.status(200).json(pkmn) : res.status(404).send("Pokemon no encontrado");
+    (pkmn.length > 0) ? res.status(200).json({code: 1, message: pkmn}) : res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 });
 
 module.exports = pokemon;
