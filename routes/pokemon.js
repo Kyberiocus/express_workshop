5960c3b045d0436a6f279e3ad4928ed2f3515134
Kyberiocus <jdanielv00@gmail.com>
2022-03-23 16:55:00 -0600

    created user

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -19,7 +19,7 @@ pokemon.post("/", async (req, res, next) => {
     return res.status(500).json({code: 500,message: "Campos incompletos"});
 });
 
-pokemon.delete('/:id([0-9]{1,3}', async (req, res, next) => {
+pokemon.delete("/:id([0-9]{1,3})", async (req, res, next) => {
     const query = `DELETE FROM pokemon WHERE pok_id= ${req.params.id}`;
 
     const rows = await db.query(query);
@@ -30,7 +30,7 @@ pokemon.delete('/:id([0-9]{1,3}', async (req, res, next) => {
     return res.status(404).json({Code:404, message:"Pokemon no encontrado"});
 });
 
-pokemon.put('/:id([0-9]{1,3}', async (req, res, next) => {
+pokemon.put("/:id([0-9]{1,3})", async (req, res, next) => {
     const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
 
     if(pok_name && pok_height && pok_weight && pok_base_experience){
@@ -47,7 +47,7 @@ pokemon.put('/:id([0-9]{1,3}', async (req, res, next) => {
     return res.status(500).json({code: 500,message: "Campos incompletos"});
 });
 
-pokemon.patch('/:id([0-9]{1,3}', async (req, res, next) => {
+pokemon.patch("/:id([0-9]{1,3})", async (req, res, next) => {
     if(req.body.pok_name){
         let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}'WHERE pok_id=${req.params.id};`;
         const rows = await db.query(query);
@@ -60,7 +60,7 @@ pokemon.patch('/:id([0-9]{1,3}', async (req, res, next) => {
     return res.status(500).json({Code:500, message:"Campos incompletos"})
 });
 
-pokemon.get("/", async (req, res, next) => {
+pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
     return res.status(200).json({code: 1, message: pkmn});
 })
