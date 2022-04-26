a3c8e0d4cdef65d40c832a01d15904a792306ec6
Kyberiocus <jdanielv00@gmail.com>
2022-04-25 19:14:51 -0500

    connecting to the user interface 3

--- routes/user.js
+++ routes/user.js
@@ -21,22 +21,23 @@ user.post("/signin", async (req, res, next) => {
 });
 
 user.post("/login", async (req,res,next) =>{
-    const {user_mail,user_password} = req.body;
+    const {user_mail, user_password} = req.body
     const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
     const rows = await db.query(query);
+
     if(user_mail && user_password){
         if(rows.lenght==1){
             const token = jwt.sign({
                 user_id: rows[0].user_id,
                 user_mail: rows[0].user_mail
             }, "debugkey");
-            return res.status(200).json({code:200, message: token});
+            return res.status(200).json({ code:200, message: token });
         }
         else{
-            return res.status(200).json({code:401, message:"Usuario y/o contraseña incorrectos"});
+            return res.status(200).json({ code:401, message:"Usuario y/o contraseña incorrectos" });
         }
     }
-    return res.status(500).json({code:500, message:"Campos incompletos"});
+    return res.status(500).json({ code:500, message:"Campos incompletos" });
 });
 
 user.get("/", async (req,res,next)=>{
